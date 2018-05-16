ab2529151ef4b2c05e29ba7485c01ef45ecd0f6f
Thor Muto Asmund <thorasmund@gmail.com>
2018-05-17 01:32:19 +0200

    Formatting database stuff

--- firebase.js
+++ firebase.js
@@ -74,7 +74,7 @@ function getMatchInfo(nextMatchElmnt, todaysMatchesElmnt) {
     if (todaysMatchesElmnt) {
       var result = '';
       matches.forEach(function(match) {
-        result += '> ' + match._class + ' mod ' + opponent + ' kl ' + match.startTime + ' på ' + match.place + ' <</br>';
+        result += match._class + ' mod ' + opponent + ' kl ' + match.startTime + ' på ' + match.place + '</br>';
       });
       todaysMatchesElmnt.innerHTML = result;
     }
